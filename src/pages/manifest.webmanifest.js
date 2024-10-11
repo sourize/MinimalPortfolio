78e288a17757d438d30d29f9e61c2d970321eadd
sourize <sourishchatterjee0067@gmail.com>
2024-10-11 20:00:11 +0530

    File Updated

--- src/pages/manifest.webmanifest.js
+++ src/pages/manifest.webmanifest.js
@@ -2,10 +2,10 @@ export async function GET() {
   const base = import.meta.env.BASE_URL
 
   const manifest = {
-    name: 'Astro AntfuStyle Theme',
-    short_name: 'AntfuStyle',
+    name: 'Sourish\'s Homepage ',
+    short_name: 'Sourish',
     description:
-      'A customizable, feature-rich Astro theme for blog and portfolio',
+      'An Aspiring ML Engineer',
     icons: [
       { src: `${base}icon-192.png`, type: 'image/png', sizes: '192x192' },
       { src: `${base}icon-512.png`, type: 'image/png', sizes: '512x512' },
